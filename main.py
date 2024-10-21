d52785f1cafda0dc238b632d7dfa496ea687a0b4
gggzp <93477662+gggzp@users.noreply.github.com>
2024-10-21 19:41:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -609,7 +609,7 @@ def main():
         if 'RTGCrbf_model.joblib' in st.session_state['files_content']:
             # 使用 BytesIO 来包装二进制数据
             try:
-                model_data =BytesIO(st.session_state['files_content']['RTGCrbf_model.joblib'])
+                model_data =BytesIO(files_content['RTGCrbf_model.joblib'])
                 st.write("模型已加载")
                 st.write(model_data)
                 RTGCrbf_model = joblib.load(model_data)
