8a1e7c65df23a7d6c836cf11f72ac7333f7f01f8
gggzp <93477662+gggzp@users.noreply.github.com>
2024-10-21 16:00:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -592,7 +592,7 @@ def create_FlashEva_SteamComp(TG1,TG2,FalshEvapTG2,Tout1,Tout2,FalshEvapElect,St
 
 def main():
     st.title('余热产蒸汽系统')
-    if file not in st.session_state:
+    if 'file' not in st.session_state:
         st.session_state['file'] = download_folder_from_onedrive(onedrive_shared_link)
         
     with st.sidebar:
